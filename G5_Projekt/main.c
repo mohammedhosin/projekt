0553cb3fb68aa606cca1f5f757db053035a16cf0
Ernst Reutergårdh <58370642+RTGRDH@users.noreply.github.com>
2020-04-03 17:06:01 +0200

    Initial Commit

---
+++ G5_Projekt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  G5_Projekt
+//
+//  Created by Ernst on 2020-04-03.
+//  Copyright © 2020 Ernst. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
